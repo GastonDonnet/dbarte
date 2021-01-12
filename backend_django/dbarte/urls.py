2af44b705b19c138b2da3083bf8809c93cc615ee
GastonDonnet <dgastonalain@gmail.com>
2021-01-12 18:30:46 -0300

    actualiza heroku settings

--- backend_django/dbarte/urls.py
+++ backend_django/dbarte/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

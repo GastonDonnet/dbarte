731bc62850d5a803403fb9362f21d6dd2c511f52
GastonDonnet <dgastonalain@gmail.com>
2021-01-27 18:39:33 -0300

    Deshabilita importacion lazy para rutas principales

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -7,7 +8,7 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue')
+    component: Home
   },
   {
     path: '/about',

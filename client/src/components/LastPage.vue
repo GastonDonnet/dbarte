779db54c2dfc63f62d99ded0b0ddf23bd78c1818
GastonDonnet <dgastonalain@gmail.com>
2021-01-28 11:29:17 -0300

    baja mas el boton porque lo tapaba el NavHeader

--- client/src/components/LastPage.vue
+++ client/src/components/LastPage.vue
@@ -6,7 +6,7 @@
       <img class="w-3 h-3 text-white" src="@/assets/svg/atras.svg" alt="" />
     </button>
     <button
-      class="fixed hidden sm:block top-5 left-5 p-4 bg-pink-500 rounded-full text-white opacity-75 hover:bg-pink-700"
+      class="fixed hidden sm:block top-20 left-5 p-4 bg-pink-500 rounded-full text-white opacity-75 hover:bg-pink-700"
     >
       <img class="w-3 h-3" src="@/assets/svg/atras.svg" alt="" />
     </button>
